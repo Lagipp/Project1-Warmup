babbea57ad3baaef24e84d1fb28bff29ce71e812
Miika Pynttäri <miika.pynttari@student.lut.fi>
2021-04-07 12:27:28 -0700

    Added the core of linked list to main

--- reverse.c
+++ reverse.c
@@ -11,18 +11,83 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#define MAX 512
+
+
+/*
+struct Node
+{
+	char structChar[MAX];
+	struct Node *pointerNext;
+};
+typedef struct Node Node;
+*/
+
+/*
+void print_inputfile(char filename[MAX])
+{
+	FILE *file;							// taken straight from the getline() manual page
+	char *line = NULL;						
+	size_t len = 0;
+	ssize_t nread;
+
+
+	file = fopen(filename, "r");
+	
+	if (file == NULL)
+	{
+		perror("Couldn't read from file...\n");			//CHANGE THIS!!!
+		exit(1);
+	}
+	
+	
+	
+	/* WITH ONLY USER INPUT TEXTFILE (1 cmdline argument) */
+
+/*
+	while ((nread = getline(&line, &len, file)) != -1) 			// taken straight from the getline() manual page
+	{       
+               fwrite(line, nread, 1, stdout);
+	}
+	
+	printf("\n");
+	
+}
+*/
+
+
+
+
+/*
+void write_into_outputfile(char inputfile[MAX], char outputfile[MAX])
+{
+
+
+}
+*/
+
+
+
+
 
 
 int main(int argc, char *argv[])
 {
 	printf("\n");
 	
-	
+	/*
 	FILE *file;							// taken straight from the getline() manual page
-	char *line = NULL;
+	char *line = NULL;						
 	size_t len = 0;
 	ssize_t nread;
+	*/
 	
+	/*
+	
+	Node *pointerStart;
+	Node *slider;
+	Node *pointerNew;
+	// char line[MAX];
 
 	
 	printf("== DEBUG: reading from a file ==\n");
@@ -34,21 +99,67 @@ int main(int argc, char *argv[])
 	
 	if (file == NULL)
 	{
-		perror("Couldn't read from file...\n");
+		perror("Couldn't read from file...\n");			//CHANGE THIS!!!
 		exit(1);
 	}
 	
-	while ((nread = getline(&line, &len, file)) != -1) 			// taken straight from the getline() manual page
+	
+	/* WITH ONLY USER INPUT TEXTFILE (1 cmdline argument) */
+	/*
+	
+		while ((nread = getline(&line, &len, file)) != -1) 			// taken straight from the getline() manual page
 	{       
                fwrite(line, nread, 1, stdout);
 	}
 	
 	printf("\n");
+	*/
+	
+	
+	/* ADDING TO LINKED LIST (2 cmdline arguments) */
+	
+	
+	
+	
+	/*
+	while ((nread = getline(&line, &len, file)) != -1) 			// taken straight from the getline() manual page
+	{       
+	
+		if ((pointerNew = (Node*)malloc(sizeof(Node))) == NULL)
+		{
+			perror("Memory allocation failed...\n");		//CHANGE THIS!!!
+			exit(1);
+		}
+		
+               pointerNew->structChar = line;
+               pointerNew->pointerNext = NULL;
+               
+               if (pointerStart == NULL)
+               {
+               	pointerStart = pointerNew;
+               } else
+               {
+               	slider = pointerStart;
+               	while (slider->pointerNext != NULL)
+               	{
+               		slider = slider->ponterNext;
+               	}
+               	slider->pointerNext = pointerNew;
+               }
+	}
+	
+	*/
+	
+	
+	
+	printf("\n");
 	printf("== DEBUG: after reading every line from file ==\n");	
 	
 	
-
+	/*
 	fclose(file);
+	*/
+	
 	
 	printf("Kiitos ohjelman käytöstä :-D\n");
 	exit(0);
