6c54dff78f23ac2103e79750747c98208873b112
Miika Pynttäri <miika.pynttari@student.lut.fi>
2021-04-07 04:05:21 -0700

    First commit :D

---
+++ reverse.c
@@ -0,0 +1,19 @@
+/* KÄYTTÖJÄRJESTELMÄT JA SYSTEEMIOHJELMOINTI
+/* Project 1: Warmup to C and Unix programming
+/* Nimi ja opiskelijanro:   Miika Pynttäri, 0563090
+/* Yhteistyö ja lähteet:   <>
+*/
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+int main(int argc, char *argv[])
+{
+
+
+
+	printf("Kiitos ohjelman käytöstä :-D\n");
+}
